329c323ffde70bc5990eed55f9be6bf2822727a5
Kohshi Yamaguchi <kyamaguc@c1r18s3.42tokyo.jp>
2023-05-08 19:27:23 +0900

    simplify sorting function

--- push_swap.c
+++ push_swap.c
@@ -22,36 +22,63 @@ void	sort_when_three(t_info **info_a)
 	{
 		reverse_rotate_a(&((*info_a)->stack_head));
 		swap_a(&((*info_a)->stack_head));
-		return ;
 	}
 	if (list->next->coordinate == (*info_a)->min && list->next->next->coordinate == (*info_a)->max)
-	{
 		swap_a(&((*info_a)->stack_head));
-		return ;
-	}
 	if (list->next->coordinate == (*info_a)->max && list->next->next->coordinate == (*info_a)->min)
-	{
 		reverse_rotate_a(&((*info_a)->stack_head));
-		return ;
-	}
 	if (list->coordinate == (*info_a)->max && list->next->coordinate == (*info_a)->min)
-	{
 		rotate_a(&((*info_a)->stack_head));
-		return ;
-	}
 	if (list->coordinate == (*info_a)->max && list->next->next->coordinate == (*info_a)->min)
 	{
 		rotate_a(&((*info_a)->stack_head));
 		swap_a(&((*info_a)->stack_head));
-		return ;
+	}
+}
+
+void	sort_from_four_to_six(t_info *info_a, t_info *info_b)
+{
+	while (info_a->size > 3)
+	{
+		if (search_forward(info_a, info_a->min) <= search_backward(info_a, info_a->min))
+		{
+			while (info_a->stack_head->coordinate != info_a->min)
+				rotate_a(&(info_a->stack_head));
+		}
+		else
+		{
+			while (info_a->stack_head->coordinate != info_a->min)
+				reverse_rotate_a(&(info_a->stack_head));
+		}
+		push_b(info_a, info_b);
+	}
+	sort_when_three(&info_a);
+	while (info_b->size)
+	{
+		push_a(info_a, info_b);
+	}
+}
+
+void	sort_above_seven(t_info *info_a, t_info *info_b)
+{
+	int		pivot;
+	size_t	element;
+
+	element = info_a->size / 6;
+	pivot = 0;
+	while (info_a->size)
+	{
+		pivot += element;
+		find_less_than_pivot_and_push_b(info_a, info_b, pivot, element);
+	}
+	while (info_b->size)
+	{
+		find_largest_and_push_a(info_a, info_b);
 	}
 }
 
 void	sort_by_input_number(t_info *info_a, t_info *info_b)
 {
-	int		pivot;
-	size_t	element;
-
 	if (check_if_list_is_sorted(*info_a) == 1)
 		return ;
 	if (info_a->size == 2)
@@ -59,41 +86,9 @@ void	sort_by_input_number(t_info *info_a, t_info *info_b)
 	if (info_a->size == 3)
 		sort_when_three(&info_a);
 	if (3 < info_a->size && info_a->size < 7)
-	{
-		while (info_a->size > 3)
-		{
-			if (search_forward(info_a, info_a->min) <= search_backward(info_a, info_a->min))
-			{
-				while (info_a->stack_head->coordinate != info_a->min)
-					rotate_a(&(info_a->stack_head));
-			}
-			else
-			{
-				while (info_a->stack_head->coordinate != info_a->min)
-					reverse_rotate_a(&(info_a->stack_head));
-			}
-			push_b(info_a, info_b);
-		}
-		sort_when_three(&info_a);
-		while (info_b->size)
-		{
-			push_a(info_a, info_b);
-		}
-	}
+		sort_from_four_to_six(info_a, info_b);
 	if (info_a->size >= 7)
-	{
-		element = info_a->size / 6;
-		pivot = 0;
-		while (info_a->size)
-		{
-			pivot += element;
-			find_less_than_pivot_and_push_b(info_a, info_b, pivot, element);
-		}
-		while (info_b->size)
-		{
-			find_largest_and_push_a(info_a, info_b);
-		}
-	}
+		sort_above_seven(info_a, info_b);
 }
 
 int main(int argc, char *argv[])
