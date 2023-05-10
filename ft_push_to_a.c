e529581c9c66f65a0986055e4a571cf8f71ac0e0
Kohshi Yamaguchi <kyamaguc@c1r18s3.42tokyo.jp>
2023-05-10 21:39:45 +0900

    swap 2 and 3

--- ft_push_to_a.c
+++ ft_push_to_a.c
@@ -7,19 +7,32 @@ int	find_largest_or_second_largest(t_stack *list, int max)
 	backward = list->prev->prev;
 	while (list->index != backward->index)
 	{
-		// if (list->index == max || list->index == (max - 1))
-		// if (list->index == max || list->index == (max - 1) || list->index == (max - 2))
-		// if (list->index == max || list->index == (max - 1) || list->index == (max - 2) || list->index == (max - 3))
 		if (list->index == max || list->index == (max - 1) || list->index == (max - 2))
 			return (list->index);
-		// if (backward->index == max || backward->index == (max - 1))
-		// if (backward->index == max || backward->index == (max - 1) || list->index == (max - 2) || list->index == (max - 3))
 		if (backward->index == max || backward->index == (max - 1) || list->index == (max - 2))
 			return (backward->index);
 		list = list->next;
 		backward = backward->prev;
 	}
 	return (list->index);
+}
+
+void swap_second_and_third(t_info *info_a, t_info *info_b)
+{
+	int		target;
+	bool	forward;
+
+	target = find_largest_or_second_largest(info_b->head, info_b->max);
+	forward = search_forward(info_b->head, target) <= search_backward(info_b->head, target);
+	if (forward)
+		rotate_a_and_b(&(info_a->head), &(info_b->head));
+	else
+		rotate_a(&(info_a->head));
+	swap_a(&(info_a->head));
+	if (forward)
+		reverse_rotate_a(&(info_a->head));
+	else
+		reverse_rotate_a_and_b(&(info_a->head), &(info_b->head));
 }
 
 void	find_largest_and_push_a(t_info *info_a, t_info *info_b)
@@ -40,23 +53,8 @@ void	find_largest_and_push_a(t_info *info_a, t_info *info_b)
 	push_a(info_a, info_b);
 	if (info_a->head->next->index != 0 && info_a->head->next->index < info_a->head->index)
 		swap_a(&(info_a->head));
-	// /*
 	if (info_a->head->next->next != 0 && info_a->head->next->next->index < info_a->head->next->index)
-	{
-		target = find_largest_or_second_largest(info_b->head, info_b->max);
-		if (search_forward(info_b->head, target) <= search_backward(info_b->head, target))
-			rotate_a_and_b(&(info_a->head), &(info_b->head));
-		else
-			rotate_a(&(info_a->head));
-
-		swap_a(&(info_a->head));
-
-		if (search_forward(info_b->head, target) <= search_backward(info_b->head, target))
-			reverse_rotate_a(&(info_a->head));
-		else
-			reverse_rotate_a_and_b(&(info_a->head), &(info_b->head));
-	}
-	// */
+		swap_second_and_third(info_a, info_b);
 }
 
 /*
